b2346f4109f48d237c767ec7b6756c79024124af
moehamieh <166070302+moehamieh@users.noreply.github.com>
2026-03-24 23:30:17 -0400

    changes

--- app/Filament/Resources/Therapists/Schemas/TherapistForm.php
+++ app/Filament/Resources/Therapists/Schemas/TherapistForm.php
@@ -6,6 +6,7 @@ use App\PublishStatus;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
@@ -17,61 +18,78 @@ class TherapistForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('role')
-                    ->required(),
-                FileUpload::make('image')
-                    ->disk('public')
-                    ->label('Image 1024 * 710')
-                    ->image()
-                    ->directory('therapists')
-                    ->visibility('public'),
-                TextInput::make('phone')
-                    ->tel(),
-                TextInput::make('office_phone')
-                    ->tel(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email(),
-                RichEditor::make('bio')
-                    ->columnSpanFull(),
-                Repeater::make('education')
+                Section::make('Information')
                     ->schema([
-                        TextInput::make('label')
+                        TextInput::make('name')
                             ->required(),
-                        TextInput::make('value')
+                        TextInput::make('role')
                             ->required(),
-                    ])
-                    ->columns(2)
-                    ->columnSpanFull(),
-                TextInput::make('certification'),
-                TextInput::make('expertise'),
-                TextInput::make('practice_years')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                Repeater::make('social_links')
+                        FileUpload::make('image')
+                            ->disk('public')
+                            ->label('Image 1024 * 710')
+                            ->image()
+                            ->directory('therapists')
+                            ->visibility('public'),
+                        TextInput::make('phone')
+                            ->tel(),
+                        TextInput::make('office_phone')
+                            ->tel(),
+                        TextInput::make('email')
+                            ->label('Email address')
+                            ->email(),
+                        RichEditor::make('bio')
+                            ->columnSpanFull(),
+                        Select::make('status')
+                            ->options(PublishStatus::class)
+                            ->default(PublishStatus::Published->value)
+                            ->required(),
+                    ])->columns(2),
+
+                Section::make('Education')
                     ->schema([
-                        Select::make('platform')
-                            ->options([
-                                'facebook' => 'Facebook',
-                                'twitter' => 'Twitter',
-                                'linkedin' => 'LinkedIn',
-                                'instagram' => 'Instagram',
+                        Repeater::make('education')
+                            ->schema([
+                                TextInput::make('label')
+                                    ->required(),
+                                TextInput::make('value')
+                                    ->required(),
                             ])
-                            ->required(),
-                        TextInput::make('url')
-                            ->url()
-                            ->required(),
-                    ])
-                    ->columns(2)
-                    ->columnSpanFull(),
-                TagsInput::make('tags'),
-                Select::make('status')
-                    ->options(PublishStatus::class)
-                    ->default(PublishStatus::Published->value)
-                    ->required(),
+                            ->columns(2)
+                            ->columnSpanFull(),
+                        Section::make('Certification & Experience')
+                            ->schema([
+                                TextInput::make('certification'),
+                                TextInput::make('expertise'),
+                                TextInput::make('practice_years')
+                                    ->required()
+                                    ->numeric()
+                                    ->default(0),
+                            ])->columns(2),
+                    ]),
+
+
+
+                Section::make('Social Links & Tags')
+                    ->schema([
+                        Repeater::make('social_links')
+                            ->schema([
+                                Select::make('platform')
+                                    ->options([
+                                        'facebook' => 'Facebook',
+                                        'twitter' => 'Twitter',
+                                        'linkedin' => 'LinkedIn',
+                                        'instagram' => 'Instagram',
+                                    ])
+                                    ->required(),
+                                TextInput::make('url')
+                                    ->url()
+                                    ->required(),
+                            ])
+                            ->columns(2)
+                            ->columnSpanFull(),
+                        TagsInput::make('tags'),
+                    ]),
+
             ]);
     }
 }
